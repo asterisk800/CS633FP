d0f2f1f629d79c498e5ff5f87911f37d7fe0a563
Gudina H <asterisk800@users.noreply.github.com>
2016-05-24 17:30:38 -0400

    Added the the MySQL database connection configuration

---
+++ config/db.js
@@ -0,0 +1,15 @@
+var Bookshelf = require('bookshelf');
+var config = {
+    host: 'localhost'
+    user: 'cs633fp',
+    password: 'cs633fp',
+    database: 'cs633fp',
+    charset: 'UTF_GENERAL_CI'
+};
+
+var cs633fp_db = Bookshelf.initialize({
+    client: 'mysql',
+    connection: config
+});
+
+module exports.cs633fp_db = cs633fp_db;
