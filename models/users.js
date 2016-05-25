11394a99fbb305a8eb080eb91a7e1bd87ba1bfc3
Gudina H <asterisk800@users.noreply.github.com>
2016-05-25 17:51:15 -0400

    Added connection termination logic.

--- models/users.js
+++ models/users.js
@@ -8,8 +8,14 @@ DB.cs633fp_db.connect(function(err) {
     console.log('Connected as id:' + DB.cs633fp_db.threadId)
 });
 
+
 DB.cs633fp_db.query('select * from cs633fp.user;', function(err, results, fields) {
     if(err) throw err;
     console.log(results);
-    });
+});
+
+
+DB.cs633fp_db.end(function (err) {
+    console.log('Thread ID:' + DB.cs633fp_db.threadId + ' has been terminated')
+});
 
