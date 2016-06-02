05b5ffa04fbb06820ce00d1b5f2908dea2482b9b
Gudina H <asterisk800@users.noreply.github.com>
2016-06-02 17:57:27 -0400

    made minor update

--- config/db.js
+++ config/db.js
@@ -1,11 +1,12 @@
 var mysql = require('mysql');
 
-var cs633fp_db = mysql.createConnection({
+module.exports = mysql.createConnection(
+    {
     host: 'localhost',
     user: 'cs633fp',
     password: 'cs633fp',
     database: 'cs633fp',
-    charset  : 'utf8'
-}); 
-
-exports.cs633fp_db = cs633fp_db;
+    charset: 'utf8'
+    }
+);
+    
