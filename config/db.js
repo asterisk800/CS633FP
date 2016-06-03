7ba660b2bbecb761de13f08c61f89a36ecd98261
Gudina H <asterisk800@users.noreply.github.com>
2016-06-03 00:45:06 -0400

    Updated passwored

--- config/db.js
+++ config/db.js
@@ -1,12 +1,12 @@
+
 var mysql = require('mysql');
 
 module.exports = mysql.createConnection(
     {
-    host: 'localhost',
-    user: 'cs633fp',
-    password: 'cs633fp',
-    database: 'cs633fp',
-    charset: 'utf8'
+        host: 'localhost',
+        user: 'cs633fp',
+        password: 'cs633fp',
+        database: 'cs633fp',
+        charset: 'utf8'
     }
 );
-    
