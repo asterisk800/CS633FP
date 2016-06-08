8cb9f1d2a9b2c3b5449dbee7f014114916805128
Gudina H <asterisk800@users.noreply.github.com>
2016-06-08 09:11:55 -0400

    Added routes for features and about-us.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,18 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+  res.render('index', {title: 'Home'});
+});
+
+
+// Features
+router.get('/features', function(req, res){
+  res.render('features', {title: 'Features'});
+});
+
+// About-us
+router.get('/about-us', function(req, res){
+  res.render('about-us', {title: 'About Us'});
 });
 
 module.exports = router;
