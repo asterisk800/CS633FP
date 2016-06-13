431422f08040f353aeb4f678dbc606ba5f3bff6c
Doug Chambers <douglas.ch@darksideproductions.net>
2016-06-13 09:18:52 -0400

    adding path

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var bodyParser = require('body-parser');
 var morgan = require('morgan');
 var app      = express();
 var port     = process.env.PORT || 3000;
+var path = require('path');
 
 var passport = require('passport');
 var flash    = require('connect-flash');
